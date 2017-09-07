76f2dc46f8e5d17e616ca213701f972e865621cb
joneshf <jones3.hardy@gmail.com>
2017-09-07 06:17:53 -0700

    Throw a LIMIT in there

--- src/Main.purs
+++ src/Main.purs
@@ -4,11 +4,11 @@ import Prelude
 
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import SQL (type (#), FROM(..), SELECT(SELECT), toSQL)
+import SQL (type (#), FROM(..), LIMIT(..), S, SELECT(..), Z, toSQL)
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-  let query :: SELECT (foo :: Int, bar :: String, baz :: Boolean) # FROM "quux"
-      query = SELECT # FROM
+  let query :: SELECT (foo :: Int, bar :: String) # FROM "quux" # LIMIT (S (S Z))
+      query = SELECT # FROM # LIMIT
 
   log $ toSQL query
--- src/SQL.purs
+++ src/SQL.purs
@@ -5,6 +5,8 @@ import Data.List ((:))
 import Data.List.Types (List)
 import Data.Monoid (mempty)
 import Data.Semigroup ((<>))
+import Data.Semiring ((+))
+import Data.Show (show)
 import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
 import Type.Row (class RowToList, Cons, Nil, RLProxy(..), kind RowList)
 
@@ -13,6 +15,9 @@ data SELECT (columns :: # Type)
 
 data FROM (table :: Symbol) a
   = FROM a
+
+data LIMIT (count :: Nat) a
+  = LIMIT a
 
 type ApplyFlipped x f = f x
 
@@ -37,6 +42,14 @@ instance toSQLFROM
     toSQL (FROM sql) =
       toSQL sql <> " FROM " <> reflectSymbol (SProxy :: SProxy table)
 
+instance toSQLLIMIT
+  :: ( ToInt count
+     , ToSQL sql
+     )
+  => ToSQL (LIMIT count sql) where
+    toSQL (LIMIT sql) =
+      toSQL sql <> " LIMIT " <> show (toInt (NProxy :: NProxy count))
+
 class ToSQLSELECT (columns :: RowList) where
   toColumn :: RLProxy columns -> List String
 
@@ -50,3 +63,21 @@ instance toSQLSELECTCons
   => ToSQLSELECT (Cons column don't_care rest) where
     toColumn _ =
       reflectSymbol (SProxy :: SProxy column) : toColumn (RLProxy :: RLProxy rest)
+
+foreign import kind Nat
+
+foreign import data Z :: Nat
+
+foreign import data S :: Nat -> Nat
+
+data NProxy (nat :: Nat)
+  = NProxy
+
+class ToInt (nat :: Nat) where
+  toInt :: NProxy nat -> Int
+
+instance toIntZ :: ToInt Z where
+  toInt _ = 0
+
+instance toIntS :: (ToInt n) => ToInt (S n) where
+  toInt _ = 1 + toInt (NProxy :: NProxy n)
